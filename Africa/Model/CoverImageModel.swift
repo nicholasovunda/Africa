17b9c9320aa27eaa8eb303f29cc334a247da1997
nicholas <nicholasovunda@gmail.com>
2022-06-13 16:28:20 +0100

    decoded local json files

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Nicholas on 13/06/2022.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
